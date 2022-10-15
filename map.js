0d9d79a67ac897c28bc73f4192da4cea55d5dec0
susanna <susanna.trollvad@gmail.com>
2022-10-15 22:44:30 +0200

    Popup

--- map.js
+++ map.js
@@ -7,11 +7,12 @@ fetch('./data/connections.json')
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
         connections.forEach(connection => {
-            L.marker([connection.stop1.latitude, connection.stop1.longitude]).addTo(map);
-            L.marker([connection.stop2.latitude, connection.stop2.longitude]).addTo(map);
-            });
-        connections.map(con => {
-            var latLng = [[con.stop1.latitude, con.stop1.longitude], [con.stop2.latitude, con.stop2.longitude]];
+            var m1 = L.marker([connection.stop1.latitude, connection.stop1.longitude]).addTo(map);
+            m1.bindPopup("<h3>" + connection.stop1.name + "</h3><div>" + connection.stop1.startYear + "</div><div>" + connection.stop1.description + "</div>");
+            var m2 = L.marker([connection.stop2.latitude, connection.stop2.longitude]).addTo(map);
+            m2.bindPopup("<h3>" + connection.stop2.name + "</h3><div>" + connection.stop2.startYear + "</div><div>" + connection.stop2.description + "</div>");
+
+            var latLng = [[connection.stop1.latitude, connection.stop1.longitude], [connection.stop2.latitude, connection.stop2.longitude]];
             L.polyline(latLng).addTo(map);
         });
     });
